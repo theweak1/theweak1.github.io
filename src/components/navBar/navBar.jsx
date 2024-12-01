e44aff9397b5ca75d3f3d5e07fdc314ef569b289
Lenier Ortiz <54192657+theweak1@users.noreply.github.com>
2024-12-01 02:09:57 -0400

    modified how it would be handle to access the blog site.

--- src/components/navBar/navBar.jsx
+++ src/components/navBar/navBar.jsx
@@ -4,29 +4,32 @@ import routes from "../../Routes/routes";
 const NavBar = ({ currentPath }) => {
   return (
     <nav className="sticky top-0 bg-bg-main dark:bg-bg-main-dark dark:text-text-dark flex justify-around items-center">
-      {routes.map((route) =>
-        route.hide ? null : route.external ? (
+      {routes.map((route) => {
+        if (route.hide) return null;
+
+        return route.external ? (
           <a
             key={route.path}
-            href={route.path}
+            href={route.path} // Ensures exact external URL is used
             target="_blank"
             rel="noopener noreferrer"
-            className="white hover:text-accent"
+            className="white"
           >
             {route.name}
           </a>
         ) : (
           <Link
             key={route.path}
-            to={route.path}
+            to={route.path} // Internal navigation handled by React Router
             className={route.path === currentPath ? "text-accent" : "white"}
           >
             {route.name}
           </Link>
-        ),
-      )}
+        );
+      })}
     </nav>
   );
 };
 
 export default NavBar;
+
